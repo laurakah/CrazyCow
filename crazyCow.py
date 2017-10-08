8a4076410bc5080c1898213e4f53a6f8aaa53fc6
Luci <mick@d17.de>
2017-10-08 16:32:16 +0200

    initial commit, v1

---
+++ crazyCow.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import time
+
+state = 0
+timer = None
+
+def initialize():
+	# TODO: switch to hour:minutes format when 60 minutes are reached
+	global state
+	global timer
+	state = 0
+	timer = 3
+	while state == 0:
+		input = raw_input(timer)
+		if input == "+":
+			timer += 1
+		elif input == "-":
+			if timer >= 2:
+				timer -= 1
+			else:
+				print "Your timer value has to be one minute or more."
+		else:
+			state = 1
+	print "Your timer is set to %d minutes." % timer
+	
+def run(timer):
+	global state
+	while state == 1:
+		print "Drink again in %d minutes." % timer
+		timer -= 1
+		if timer == 0:
+			state = 2
+			print "Drink!"
+		time.sleep(6)
+		
+def alarm():
+	global state
+	runsound = "sound.py"
+	while state == 2:
+		execfile(runsound)
+		alarmInput = raw_input("Did you drink?")
+		if alarmInput == "y":
+			state = 1
+
+
+def main():
+	while True:
+		if state == 0:
+			initialize()
+		elif state == 1:
+			run(timer)
+		elif state == 2:
+			alarm()
+main()
